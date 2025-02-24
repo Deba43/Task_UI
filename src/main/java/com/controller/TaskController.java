f5260bf82f1ac5c8b418864cc4f96d33f602253f
Deba43 <debadatta016@gmail.com>
2025-02-24 22:21:56 +0530

    code

---
+++ src/main/java/com/controller/TaskController.java
@@ -0,0 +1,49 @@
+package com.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.client.RestTemplate;
+
+import com.entity.Task;
+
+@Controller
+public class TaskController {
+
+    private final String BASE_URL = "http://localhost:7777";
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/")
+    public String home() {
+        return "home"; 
+    }
+
+    @GetMapping("/createTasks")
+    public String createTask(Model model) {
+        model.addAttribute("task",new Task());
+        return "addTask";
+    }
+
+    @PostMapping("/addTask")
+    public String createTasks(@ModelAttribute Task task, Model model) {
+        ResponseEntity<Task> response = restTemplate.postForEntity(
+                BASE_URL + "/addTask", task, Task.class); 
+        model.addAttribute("message", "Task added successfully: " + response.getBody().getTitle());
+        return "addTask"; 
+
+    }
+
+    
+
+    @GetMapping("/viewTasks")
+    public String displayTasks() {
+        return "viewTasks"; 
+    }
+
+}
---
+++ src/main/java/com/entity/Task.java
@@ -0,0 +1,20 @@
+package com.entity;
+
+
+import java.time.LocalDate;
+import lombok.Data;
+
+@Data
+public class Task {
+    private String title;
+    private String description;
+    private String location;
+    private String status;
+    private String priority;
+    private String category;
+
+    private LocalDate eventDate;
+    private LocalDate createdAt;
+    private LocalDate endAt;
+    private LocalDate updatedAt; 
+}
