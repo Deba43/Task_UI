623ca22cde2d9b357d8b68f2e41243ae64c75a17
Deba43 <debadatta016@gmail.com>
2025-03-01 19:37:48 +0530

    code

--- src/main/java/com/controller/TaskController.java
+++ src/main/java/com/controller/TaskController.java
@@ -1,6 +1,9 @@
 package com.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,29 +24,57 @@ public class TaskController {
 
     @GetMapping("/")
     public String home() {
-        return "home"; 
+        return "home";
     }
 
     @GetMapping("/createTasks")
     public String createTask(Model model) {
-        model.addAttribute("task",new Task());
+        model.addAttribute("task", new Task());
         return "addTask";
     }
 
-    @PostMapping("addTask")
+    @PostMapping("/addTask")
     public String createTasks(@ModelAttribute Task task, Model model) {
         ResponseEntity<Task> response = restTemplate.postForEntity(
-                BASE_URL + "/addTask", task, Task.class); 
+                BASE_URL + "/addTask", task, Task.class);
         model.addAttribute("message", "Task added successfully: " + response.getBody().getTitle());
-        return "addTask"; 
+        return "addTask";
 
     }
-
-    
 
     @GetMapping("/viewAllTasks")
-    public String displayTasks() {
-        return "viewTasks"; 
+    public String displayTasks(@ModelAttribute Task task, Model model) { 
+
+        String url = BASE_URL + "/viewAllTasks";
+
+        ResponseEntity<List<Task>> response = restTemplate.exchange(
+                url,
+                org.springframework.http.HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<Task>>() {
+                });
+        List<Task> tasks = response.getBody();
+
+        model.addAttribute("tasks", tasks);
+
+        return "viewTasks";
     }
 
+    @GetMapping("/searchTasks")
+    public String searchTasks(@ModelAttribute Task task, Model model) { // SearchAllTasks
+
+        String url = BASE_URL + "/viewAllTasks";
+
+        ResponseEntity<List<Task>> response = restTemplate.exchange(
+                url,
+                org.springframework.http.HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<Task>>() {
+                });
+        List<Task> tasks = response.getBody();
+
+        model.addAttribute("tasks", tasks);
+
+        return "searchTasks";
+    }
 }
